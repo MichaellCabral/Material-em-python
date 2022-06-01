44f35187460b33145bb87f891696ce1ecef66eaf
MichaellCabral <michaellbritotec@gmail.com>
2022-06-01 16:56:45 -0300

    update work
    lista num

--- Exercicios ead/aula 006 lista de numeros.py
+++ Exercicios ead/aula 006 lista de numeros.py
@@ -1,4 +1,8 @@
 lista_num = [10,20,30,40,50]
-print(lista_num)
-print(min = (lista_num))
-
+print('Todos os elementos da lista sao {}'.format(lista_num))
+min(lista_num)
+print('o numero minimo da lista eh: {}'.format(min(lista_num)))#mostra o numero minimo da lista
+max(lista_num)
+print('o numero maximo da lista eh: {}'.format(max(lista_num)))#mostra o numero maximo da lista
+sum(lista_num)
+print('a soma de todos os numeros da lista eh: {}'.format(sum(lista_num)))#mostra a soma de todos os numeros da lista
