ed29ee4cf0c8f0d8ce7ee35d309f16a5f4938157
MichaellCabral <michaellbritotec@gmail.com>
2022-06-20 17:56:20 -0300

    update funcoes e parametros

---
+++ Exercicios curso em video/Curso Python #20 - Funções (Parte 1)PARAMETROS.py
@@ -0,0 +1,12 @@
+def mostrar_linhas(): # tem que executar o cabeçalho pra mostrar sua instrucao
+    print(50*"-")
+
+
+# Entre o def e o programa principal tem que ter duas linhas, como acima. (questao de estetica).
+mostras_linhas()
+print('Capitulo 1')
+mostrar_linha()
+
+mostrar_linhas()
+print('capitulo 2')
+mostrar_linhas()()
