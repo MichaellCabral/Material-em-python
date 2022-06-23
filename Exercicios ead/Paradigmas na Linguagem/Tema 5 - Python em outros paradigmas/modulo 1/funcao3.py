de3ba09f339e05ba8739f703a809fc2b0a0bd7ee
MichaellCabral <michaellbritotec@gmail.com>
2022-06-23 17:58:10 -0300

    update

--- Exercicios ead/Paradigmas na Linguagem/Tema 5 - Python em outros paradigmas/modulo 1/funcao3.py
+++ Exercicios ead/Paradigmas na Linguagem/Tema 5 - Python em outros paradigmas/modulo 1/funcao3.py
@@ -1,8 +1,11 @@
 valores = [10, 20, 30]
+print('lista original:',valores)
 
 def altera_lista(lista):
     lista[2] = lista[2] + 10
+    
     return lista
+    
 
 print("Nova lista", altera_lista(valores))
 print("Nova lista", altera_lista(valores))
--- Exercicios ead/Paradigmas na Linguagem/Tema 5 - Python em outros paradigmas/modulo 1/funcao4.py
+++ Exercicios ead/Paradigmas na Linguagem/Tema 5 - Python em outros paradigmas/modulo 1/funcao4.py
@@ -7,3 +7,5 @@ def altera_lista(lista):
 
 print("Nova lista", altera_lista(valores))
 print("Nova lista", altera_lista(valores))
+
+#No exemplo do script funcao4.py, muito similar ao anterior, ao invés de alterarmos o valor do próprio parâmetro, criamos uma cópia dele (linha 4), sendo assim, não alteramos a variável valores e obtemos o mesmo resultado para as duas chamadas da função (linha 8 e 9).
