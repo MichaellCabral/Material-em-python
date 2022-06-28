ecf39c17cedb86e82e70a30530214d1fee57c506
MichaellCabral <michaellbritotec@gmail.com>
2022-06-28 17:51:26 -0300

    update work

---
+++ Exercicios curso em video/ex025 Procurando uma string dentro de outra.py
@@ -0,0 +1,9 @@
+nome = str(input('Qual seu nome completo? ')).strip()
+print(f'Seu nome tem Silva? {"SILVA" in nome.upper()}')
+
+#Exercício Python 025: Crie um programa que leia o nome de uma pessoa e diga se ela tem "SILVA" no nome, ou seja, em qualquer lugar.
+#A resposta mostra verdadeiro ou falso (true or false)
+
+# linha 1 - retira os espaços ao redor
+# linha 2 - O nome "SILVA" em letras maiusculas retira o erro de alguma letra ser digitada e der falso no final. pois ele transforma todas as letras em maiusculas com o "nome.upper" pra verificar o resultado todo em maisculo
+#O "in" nao e um metodo, e sim um operador
