927f44eca4f4563159b7a4193258c15d7f6c9a97
MichaellCabral <michaellbritotec@gmail.com>
2022-06-30 23:03:03 -0300

    update home
    python feito

--- Exercicios curso em video/ex024 Verificando as primeiras letras de um texto.py
+++ Exercicios curso em video/ex024 Verificando as primeiras letras de um texto.py
@@ -1,3 +1,8 @@
+city = input("Digite uma cidade: \n").lower().strip().split() 
+print(city)
+print(city[0] == "santos")
+
 # Exercício Python 024: Crie um programa que leia o nome de uma cidade diga se ela começa ou não com o nome "SANTO"
-cid =  str(input('Em que cidade você nasceu? ')).strip()
-print(cid[:5].upper() == 'Santos')
+# Linha 1 não precisa do str (sem ele o programa identifica como string)
+# o lower transforma a string em maiuscula , o strip tira os espaços antes e depois da strg e o split transforma a string em listas
+# Linha 3 pega o primeiro valor da lista e verifica se a palavra santos esta presente independente de maiuscula ou minuscula
