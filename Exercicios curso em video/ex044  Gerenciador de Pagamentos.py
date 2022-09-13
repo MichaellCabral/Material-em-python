aee5fd60e8e934bd0b96ef83e99f34f37053acdb
Michaell Cabral Analista de Sistemas <88794323+MichaellCabral@users.noreply.github.com>
2022-09-13 17:33:11 -0300

    update
    Até o ex045 de python (finalizando if)

--- Exercicios curso em video/ex044  Gerenciador de Pagamentos.py
+++ Exercicios curso em video/ex044  Gerenciador de Pagamentos.py
@@ -1,5 +1,28 @@
 print('{:=^40}'.format('LOJAS GUANABARA'))
-
+preço = float(input('Preço das compras: R$ '))
+print('''FORMAS DE PAGAMENTO
+[ 1 ] à vista dinheiro
+[ 2 ] à vista no cartão
+[ 3 ] 2x no cartão
+[ 4 ] 3x ou mais no cartão''')
+opção = int(input('Qual é a opção? '))
+if opção == 1:
+    total = preço - (preço * 10/100)
+elif opção == 2:
+    total = preço - (preço * 5/100)
+elif opção == 3:
+    total = preço
+    parcela = total / 2
+    print(f'Sua compra será parcelada em 2x de R${parcela:.2f}')
+elif opção == 4:
+    total = preço + (preço * 20/100)
+    totparc = int(input('Quantas parcelas? '))
+    parcela = total / totparc
+    print(f'Sua compra será parcelada em {totparc}x de R${parcela:.2f} COM JUROS')
+else:
+    total = preço
+    print('OPÇÃO INVÁLIDA de pagamento. Tente novamente')
+print(f'Sua compra de R${preço:.2f} vai custar R$ {total:.2f} no final.')
 
 
 # Exercício Python 044: Elabore um programa que calcule o valor a ser pago por um produto, considerando o seu preço normal e condição de pagamento:
