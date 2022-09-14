1173537d93a74a1012a7910086c763f2d42083ab
Michaell Cabral Analista de Sistemas <88794323+MichaellCabral@users.noreply.github.com>
2022-09-14 17:53:59 -0300

    update

---
+++ Exercicios curso em video/ex046  Contagem regressiva.py
@@ -0,0 +1,10 @@
+from time import sleep # sleep pra fazer a contagem de tempo
+for cont in range(10,-1,-1):
+    print(cont)
+    sleep(1) #dentro da indentação pra contar número por número (1 segundo)
+print('BUM! BUM! POOOW!')
+
+
+# Exercício Python 046: Faça um programa que mostre na tela uma contagem regressiva para o estouro de fogos de artifício, indo de 10 até 0, com uma pausa de 1 segundo entre eles.
+# variavel cont na range que começa com 10 e termina em 0
+# o último passo '-1' serve para trazer a contagem em ordem decrescente
---
+++ ex048 - Soma ímpares múltiplos de três.py
@@ -0,0 +1,11 @@
+soma = 0 # acumulador
+cont = 0 # acumulador
+for c in range(1,501,2):       # listar impares entre 1 e 500
+    if c % 3 == 0:             # listar múltiplos de 3
+        soma += c # soma os valores respectivamente (soma = soma + c)
+        cont += 1 # conta mais um número 01 (cont = cont + 1)
+        #print(c, end=' ') mostra todos os números da solução
+print(f'A soma de todos os {cont} valores solicitados é {soma}')
+
+    
+# Exercício Python 048: Faça um programa que calcule a soma entre todos os números que são múltiplos de três e que se encontram no intervalo de 1 até 500.
